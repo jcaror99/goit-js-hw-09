cd449008e6f909c822ab4ffd28ae2ea699eb74d4
jcaror99 <121507887+jcaror99@users.noreply.github.com>
2023-09-27 23:26:07 -0500

    challengeOne

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const bodyColor = () => {
+  body.style.backgroundColor = getRandomHexColor();
+};
+
+const changeColor = () => {
+  bodyColor();
+  startButton.disabled = true;
+  timerId = setInterval(bodyColor, 1000);
+};
+
+const stopColor = () => {
+  clearInterval(timerId);
+  startButton.disabled = false;
+};
+
+startButton.addEventListener('click', changeColor);
+stopButton.addEventListener('click', stopColor);
